3149ef25c8c57da2b4bf0bf322b9ff7eb40a52e2
Townsend Saunders <townsend3721@berkeley.edu>
2019-04-11 18:01:54 -0700

    Fixed travis ci checks

--- spec/factories/guest.rb
+++ spec/factories/guest.rb
@@ -3,11 +3,17 @@ FactoryGirl.define do
     firstname "John"
     lastname "Smith"
     email "john@smith.com"
+    occupation "other"
+    gender "other"
+    department "other"
   end
 
   factory :invalid_guest, parent: :guest do
     firstname "John"
     lastname "Smith"
     email "notgood"
+    occupation "other"
+    gender "other"
+    department "other"
   end
 end

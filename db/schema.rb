18aede3f4962c66395c7a41631bbfeea36e63dd5
Townsend Saunders <townsend3721@berkeley.edu>
2019-05-13 20:17:07 -0700

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190509045608) do
+ActiveRecord::Schema.define(version: 20190511054614) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -68,6 +68,13 @@ ActiveRecord::Schema.define(version: 20190509045608) do
     t.string "firstname"
     t.string "lastname"
     t.string "role"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.text "description"
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
